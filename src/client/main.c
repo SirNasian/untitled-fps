029124b7dac1809cfa92f758c24e393a3fb0e934
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2026-07-04 21:39:29 +0800

    hint opengl forward compatibility for macos builds

--- src/client/main.c
+++ src/client/main.c
@@ -99,6 +99,7 @@ int main(int argc, char **argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow *window = glfwCreateWindow(800, 600, "Untitled FPS", NULL, NULL);
 	if (window == NULL)
